17c3bd0940540cfe46bfc25ecaa3de536a0ba743
lucas duarte <lucasduarteinacio@hotmail.com>
2020-05-08 20:09:03 -0300

    first commit

---
+++ src/test/java/com/ITIL/GestaoDeIncidentes/GestaoDeIncidentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITIL.GestaoDeIncidentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeIncidentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
